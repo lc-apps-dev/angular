7ec4b83981bf91fae8b8e8c80a2bcbcd138a57d9
lc-apps-dev <lc.apps.dev@gmail.com>
2021-02-03 10:32:02 +0100

    changed localization of ng4_complete_guide project

---
+++ ng4_complete_guide/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,29 @@
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/Ingredient.model';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
+})
+export class ShoppingEditComponent implements OnInit {
+
+  @ViewChild('nameInput', {static: true}) nameInputRef :ElementRef;
+  
+  @ViewChild('amountInput', {static: true}) amountInputRef :ElementRef;
+  
+  @Output() ingridientAdded = new EventEmitter<Ingredient>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingridientAdded.emit(newIngredient);
+  }
+
+}
