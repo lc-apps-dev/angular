5dc8bb145ea3035c9238816776cebe18bf0a4716
lc-apps-dev <lc.apps.dev@gmail.com>
2021-02-15 13:04:18 +0100

    ng4_complete_guide - added routing to all links

--- ng4_complete_guide/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ ng4_complete_guide/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -11,15 +11,20 @@ export class RecipeItemComponent implements OnInit {
 
   @Input() recipe : Recipe;
 
+  @Input() index: number;
+
   //@Output() recipeSelected = new EventEmitter<void>();
 
-  constructor(private recipeService: RecipeService) { }
+  
 
   ngOnInit(): void {
   }
 
+  /*
+  constructor(private recipeService: RecipeService) { }
+  
   onSelected() {
     //this.recipeSelected.emit();
     this.recipeService.recipeSelected.emit(this.recipe);
-  }
+  }*/
 }
