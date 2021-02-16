cc494efe401e15c192c15727544ba408d42a97e9
lc-apps-dev <lc.apps.dev@gmail.com>
2021-02-16 16:32:14 +0100

    ng4_complete_guide - changed Observables to Subject

--- ng4_complete_guide/src/app/recipes/recipe.service.ts
+++ ng4_complete_guide/src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { Ingredient } from "../shared/Ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
@@ -6,7 +7,7 @@ import { Recipe } from "./recipe.model";
 @Injectable()
 export class RecipeService {
 
-    recipeSelected = new EventEmitter<Recipe>();
+    recipeSelected = new Subject<Recipe>();
 
 
     constructor(private shoppingListService: ShoppingListService) {}
--- ng4_complete_guide/src/app/recipes/recipes.component.ts
+++ ng4_complete_guide/src/app/recipes/recipes.component.ts
@@ -10,17 +10,17 @@ import { RecipeService } from './recipe.service';
 })
 export class RecipesComponent implements OnInit {
 
-  selectedRecipe: Recipe;
+  //selectedRecipe: Recipe;
   
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
-
+/*
     this.recipeService.recipeSelected.subscribe(
       (recipe: Recipe) => {
         this.selectedRecipe = recipe;
       }
-    );
+    );*/
   }
 
 
