f583dd52c6e5ee742085a46c6c874215b62daa09
lc-apps-dev <lc.apps.dev@gmail.com>
2021-02-16 22:26:42 +0100

    added 15_forms_start

---
+++ 15_forms-td-start/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  @ViewChild('f') signupForm: NgForm;
+
+  defaultQuestion = 'pet';
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+
+
+  //onSubmit(form: NgForm) {
+    //console.log(form);
+  //}
+}
