730274afb7c82c57c44763ca2111d3c60c076a3f
lc-apps-dev <lc.apps.dev@gmail.com>
2021-03-02 09:27:39 +0100

    18_http-01-start - added params to request

--- 18_http-01-start/src/app/posts.service.ts
+++ 18_http-01-start/src/app/posts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Subject, throwError } from "rxjs";
 import { map, catchError } from 'rxjs/operators';
@@ -29,10 +29,15 @@ export class PostsService {
     }
 
     fetchPosts() {
+
+        let searchParams = new HttpParams();
+        searchParams = searchParams.append('print', 'preety');
+
         return this.http.get<{[key: string]: Post}>(
             'https://ng-complete-guide-3894e-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
             {
-                headers: new HttpHeaders({'Custom-Header' : 'Hello'})
+                headers: new HttpHeaders({'Custom-Header' : 'Hello'}),
+                params: searchParams
             }
             )
             .pipe(map(
