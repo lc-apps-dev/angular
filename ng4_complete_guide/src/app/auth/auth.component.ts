e2988226ab46404a6ab48ac074d1d8dc3ec06843
lc-apps-dev <lc.apps.dev@gmail.com>
2021-03-15 13:26:52 +0100

    ng4_complete_guide - dynamic components - added alert component as handled dynamically

--- ng4_complete_guide/src/app/auth/auth.component.ts
+++ ng4_complete_guide/src/app/auth/auth.component.ts
@@ -1,7 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, ComponentFactoryResolver, OnDestroy, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Router } from "@angular/router";
-import { Observable } from "rxjs";
+import { Observable, Subscription } from "rxjs";
+import { AlertComponent } from "../shared/alert/alert.component";
+import { PlaceholderDirective } from "../shared/placeholder/placeholder.directive";
 import { AuthService, AuthResponseData } from "./auth.service";
 
 
@@ -9,13 +11,21 @@ import { AuthService, AuthResponseData } from "./auth.service";
     selector: 'app-auth',
     templateUrl: './auth.component.html'
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
 
-    constructor(private authService: AuthService, private router: Router) {}
+    constructor(
+        private authService: AuthService, 
+        private router: Router,
+        private componentFactoryResolver: ComponentFactoryResolver) {}
+
 
     isLoginMode = true;
     isLoading = false;
     error: string = null;
+
+    private closeSub : Subscription;
+
+    @ViewChild(PlaceholderDirective, { static: false }) alertHost: PlaceholderDirective;
 
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
@@ -49,6 +59,7 @@ export class AuthComponent {
             errorResponse => {
                 console.error(errorResponse);
                 this.error = errorResponse;
+                this.showErrorAlert(errorResponse);
                 this.isLoading = false;
             });
         
@@ -58,6 +69,28 @@ export class AuthComponent {
 
     onHandleError() {
         this.error = null;
+    }
+
+    private showErrorAlert(message: string) {
+
+        const alertComponentFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+        const hostViewContainerRef = this.alertHost.viewContainerRef;
+        
+        hostViewContainerRef.clear();
+
+        const componentRef = hostViewContainerRef.createComponent(alertComponentFactory);
+
+        componentRef.instance.message = message;
+        this.closeSub = componentRef.instance.close.subscribe(() => {
+            this.closeSub.unsubscribe();
+            hostViewContainerRef.clear();
+        });
+    }
+
+    ngOnDestroy(): void {
+        if(this.closeSub) {
+            this.closeSub.unsubscribe();
+        }
     }
 
 }
