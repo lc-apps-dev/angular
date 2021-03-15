39b6d5ba2f0b3c27583c14e912a0f168e6ea2ee0
lc-apps-dev <lc.apps.dev@gmail.com>
2021-03-15 12:07:57 +0100

    ng4_complete_guide - auth: added auth guard

--- ng4_complete_guide/src/app/app-routing.module.ts
+++ ng4_complete_guide/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AuthComponent } from "./auth/auth.component";
+import { AuthGuard } from "./auth/auth.guard";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { RecipesResolverService } from "./recipes/recipe-resolver.service";
@@ -12,12 +13,15 @@ const appRoutes: Routes = [
 
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
 
-    { path: 'recipes', component: RecipesComponent, children: [
-        { path: '', component: RecipeStartComponent },
-        { path: 'new', component: RecipeEditComponent },
-        { path: ':id', component: RecipeDetailComponent, resolve:[RecipesResolverService] },
-        { path: ':id/edit', component: RecipeEditComponent, resolve:[RecipesResolverService] },
-    ] },
+    { path: 'recipes', component: RecipesComponent,
+        canActivate:[AuthGuard],
+        children: [
+            { path: '', component: RecipeStartComponent },
+            { path: 'new', component: RecipeEditComponent },
+            { path: ':id', component: RecipeDetailComponent, resolve:[RecipesResolverService] },
+            { path: ':id/edit', component: RecipeEditComponent, resolve:[RecipesResolverService] },
+        ] 
+    },
 
     { path: 'shopping-list', component: ShoppingListComponent },
 
