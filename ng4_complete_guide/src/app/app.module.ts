7f48114f385c4a25dff1c517323615ce1c2a3983
lc-apps-dev <lc.apps.dev@gmail.com>
2021-03-24 17:18:09 +0100

    ng4_complete_guide: modules - added recipes module

--- ng4_complete_guide/src/app/app.module.ts
+++ ng4_complete_guide/src/app/app.module.ts
@@ -18,17 +18,12 @@ import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from '@angular/common';
+import { RecipesModule } from './recipes/recipes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    RecipeStartComponent,
-    RecipeEditComponent
+    HeaderComponent
   ],
   imports: [
     ShoppingListModule,
@@ -41,7 +36,7 @@ import { CommonModule } from '@angular/common';
     AuthModule,
     FormsModule,
     CommonModule,
-    ReactiveFormsModule
+    RecipesModule
   ],
   providers: [
   ],
